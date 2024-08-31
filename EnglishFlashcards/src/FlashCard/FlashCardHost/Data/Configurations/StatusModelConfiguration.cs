02c7f6763b101ea5b07561198f9d545d38bd8802
Staso Stanislav <xctacx5@gmail.com>
2024-08-31 14:40:13 +0300

    Configure StatusModel

---
+++ EnglishFlashcards/src/FlashCard/FlashCardHost/Data/Configurations/StatusModelConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FlashCard.Host.Data.Configurations
+{
+    public class StatusModelConfiguration : IEntityTypeConfiguration<StatusModel>
+    {
+        public void Configure(EntityTypeBuilder<StatusModel> builder)
+        {
+            builder.HasKey(x => x.StatusId);
+
+            builder.Property(x => x.StatusName)
+                .HasMaxLength(30)
+                .HasColumnName("Status")
+                .IsRequired();
+        }
+    }
+}
