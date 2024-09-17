434a7caf51d82ed25f2e4755efefdb25a7936bfc
Staso Stanislav <xctacx5@gmail.com>
2024-09-17 14:03:51 +0300

    Add WordDbModelValidator

---
+++ EnglishFlashcards/src/FlashCard/FlashCardHost/Validators/WordDbModelValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace FlashCard.Host.Validators
+{
+    public class WordDbModelValidator : AbstractValidator<WordDbModel>
+    {
+        public WordDbModelValidator()
+        {
+            
+        }
+    }
+}
