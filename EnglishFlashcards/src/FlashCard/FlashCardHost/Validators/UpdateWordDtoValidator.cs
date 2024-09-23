586f44f80a8ecc4b3b1244957a87409828c9d6a3
Staso Stanislav <xctacx5@gmail.com>
2024-09-23 13:20:50 +0300

    Update UpdateWordDtoValidator

--- EnglishFlashcards/src/FlashCard/FlashCardHost/Validators/UpdateWordDtoValidator.cs
+++ EnglishFlashcards/src/FlashCard/FlashCardHost/Validators/UpdateWordDtoValidator.cs
@@ -6,9 +6,9 @@ namespace FlashCard.Host.Validators
     {
         public UpdateWordDtoValidator()
         {
-            RuleFor(x  => x.Id).NotEmpty().WithMessage("The Id can`t be empty."); ;
-            RuleFor(x => x.Value).NotEmpty().WithMessage("The Value can`t be empty."); ;
-            RuleFor(x => x.TranslateValue).NotEmpty().WithMessage("The TranslateValue can`t be empty."); ;
+            RuleFor(x  => x.Id).NotEmpty().GreaterThan(0).WithMessage("The Id can`t be empty.");
+            RuleFor(x => x.Value).NotEmpty().WithMessage("The Value can`t be empty.");
+            RuleFor(x => x.TranslateValue).NotEmpty().WithMessage("The TranslateValue can`t be empty.");
         }
     }
 }
