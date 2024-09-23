6d8ff130bdcc84500ad3b6c38285db2b886d2dd1
Staso Stanislav <xctacx5@gmail.com>
2024-09-23 14:58:59 +0300

    Create TranslateController

--- EnglishFlashcards/src/FlashCard/FlashCardHost/Controllers/FlashCardController.cs
+++ EnglishFlashcards/src/FlashCard/FlashCardHost/Controllers/FlashCardController.cs
@@ -4,15 +4,8 @@ namespace FlashCardHost.Controllers
 {
     [ApiController]
     [Route("api/flashcard")]
-    public class FlashCardController(
-        ITranslateService translate) : ControllerBase
+    public class FlashCardController() : ControllerBase
     {
-        [HttpGet("/GetTranslateWord")]
-        public async Task<IActionResult> GetTranslateWord(string text) 
-        {
-            var response = await translate.Translate(text);
-
-            return Ok(response);
-        }
+        
     }
 }
---
+++ EnglishFlashcards/src/FlashCard/FlashCardHost/Controllers/TranslateController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlashCard.Host.Controllers
+{
+    [ApiController]
+    [Route("api/translate")]
+    public class TranslateController
+        (ITranslateService translateService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Translate(string message) 
+        {
+            var result = await translateService.Translate(message);
+            return Ok(result);
+        }
+    }
+}
